624a7c5fdd828c64a9cbb0396bee54c43db74d97
Yogesh <yogeshtml.alkurn@gmail.com>
2019-04-12 11:40:34 +0530

    faq

--- resources/views/front/pages/faq.blade.php
+++ resources/views/front/pages/faq.blade.php
@@ -7,169 +7,167 @@
 
 
  <section class="faq-sect">
-
    <div class="container">
 
-      <h3>Host FAQ</h3>
+      <h3>FAQ</h3>
 
-      <div id="faq" role="tablist" aria-multiselectable="true">
+<ul class="nav nav-tabs">
+  <li class="active"><a data-toggle="tab" href="#general-faq">General</a></li>
+  <li><a data-toggle="tab" href="#host-faq">Host</a></li>
+  <li><a data-toggle="tab" href="#renter-faq">Renter</a></li>
+  <li><a data-toggle="tab" href="#pricing-faq">Pricing</a></li>
+  <li><a data-toggle="tab" href="#payments-faq">Payments</a></li>
+  <li><a data-toggle="tab" href="#other-faq">Other</a></li>
+</ul>
 
-
-
+<div class="tab-content">
+  <div id="general-faq" class="tab-pane fade in active">
+    <div id="general" role="tablist" aria-multiselectable="true">
+      
       <div class="card">
-
       <div class="card-header" role="tab" id="questionOne">
-
       <h5 class="card-title">
-
       <a data-toggle="collapse" data-parent="#faq" href="#answerOne" aria-expanded="true" aria-controls="answerOne">
-
-      What happens if I don’t accept a booking request?
-
+      What is Pryme Space?
       </a>
-
       </h5>
-
       </div>
-
       <div id="answerOne" class="collapse show" role="tabcard" aria-labelledby="questionOne">
-
       <div class="card-body">
-
-        <p>You can decline a booking request, but should do so within 24 hours. The amount of time it takes you to respond is factored into your response rate. We track your response rate to ensure reliability of our platform and community.</p>
-
+        <p>We’re the place for your space. We make finding and listing space easy. We don’t just help you park or find a place to stash that old furniture you were planning to sell. Check out our growing selection for <strong>Parking</strong>, <strong>Land</strong>, <strong>Industrial</strong>, <strong>Storage</strong>, <strong>Office</strong> and <strong>Event</strong> spaces. </p>
       </div>
-
       </div>
-
       </div>
-
-
 
       <div class="card">
-
       <div class="card-header" role="tab" id="questionTwo">
-
       <h5 class="card-title">
-
       <a class="collapsed" data-toggle="collapse" data-parent="#faq" href="#answerTwo" aria-expanded="false" aria-controls="answerTwo">
-
-        Can I list multiple categories of spaces?
-
+      What is the cancellation and refund policy?
       </a>
-
       </h5>
-
       </div>
-
       <div id="answerTwo" class="collapse" role="tabcard" aria-labelledby="questionTwo">
-
       <div class="card-body">
+        <p>Every space is different. A single cancellation policy does not fit all booking types. Hosts can choose the appropriate policy that works best for their space and business. One of the following cancellation policies chosen by Host will be applied to each listing, except for On-Demand Parking.</p>
 
-        <p>Absolutely! You can list as many as you want.</p>
+        <h6>Flexible</h6>
 
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;For a full refund of fees, cancellations must be made a full 24 hours prior to listing’s local check in time (or 3:00 PM if not specified) on the day of check in. For example, if check-in is on Friday, cancel by Thursday of that week before check in time.<br>
+
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;If the renter cancels less than 24 hours before check-in, the first night is non-refundable.<br>
+
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;If the renter arrives and decides to leave early, the accommodation fees for the nights not spent 24 hours after the official cancellation are fully refunded.</p>
+
+        <h6>Medium</h6>
+
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; For a full refund of fees, cancellations must be made five full days prior to listing’s local start time (or 3:00 PM if not specified) on the day of check in. For example, if check-in is on Friday, cancel by the previous Sunday before check in time.<br>
+
+       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;If the renter cancels less than 5 days in advance, the first night is non-refundable but 50% of the accommodation fees for remaining nights will be refunded.<br>
+
+       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;If the renter decides to vacate early, 50% of the booking fees for the nights not spent 24 hours after the cancellation occurs are refunded.</p>
+
+
+       <h6>Strict</h6>
+
+       <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;For a full refund of booking fees, cancellations must be made within 48 hours of booking and at least 14 full days prior to listing’s local check-in time (or 3:00 PM if not specified) on the day of check-in.<br>
+
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;For a 50% refund of booking fees, cancellations must be made 7 full days prior to listing’s local check in time (or 3:00 PM if not specified) on the day of check in, otherwise no refund. For example, if check-in is on Friday, cancel by Friday of the previous week before check in time.<br>
+
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;If the renter cancels less than 7 days in advance or decides to leave early after check-in, the nights not spent are not refunded.</p>
+
+
+        <h6>On-Demand Parking (booking within 24 hours)</h6>
+
+        <p>Bookings are non-refundable, regardless of your use or non-use of the Booking and regardless of any circumstance surrounding the use or non-use of a Booking (i.e., traffic or weather delays, cancellation of a related event).</p>
+
+        <p><em>*Cancellation and refund policies are subject to change*</em></p>
       </div>
-
       </div>
-
       </div>
-
 
 
       <div class="card">
-
       <div class="card-header" role="tab" id="questionThree">
-
       <h5 class="card-title">
-
       <a class="collapsed" data-toggle="collapse" data-parent="#faq" href="#answerThree" aria-expanded="true" aria-controls="answerThree">
-
-        Can I rent out my driveway?
-
+          How do I create an account to list or book a space? 
       </a>
-
       </h5>
-
       </div>
-
       <div id="answerThree" class="collapse in" role="tabcard" aria-labelledby="questionThree">
-
       <div class="card-body">
-
-          <p>Yes! Start today by clicking <a href="#" data-toggle="modal"  data-target="#singupModal">here</a>.</p>
-
+          Simply signup <a href="#" data-toggle="modal"  data-target="#singupModal">here</a> as a user or a host.
       </div>
-
       </div>
-
       </div>
-
 
 
       <div class="card">
-
       <div class="card-header" role="tab" id="questionFour">
-
       <h5 class="card-title">
-
       <a class="collapsed" data-toggle="collapse" data-parent="#faq" href="#answerFour" aria-expanded="true" aria-controls="answerFour">
-
-          When will I get paid?
-
+          What are the benefits of booking using Pryme Space?
       </a>
-
       </h5>
-
       </div>
-
       <div id="answerFour" class="collapse in" role="tabcard" aria-labelledby="questionFour">
-
       <div class="card-body">
+          <p>There are a lot of cool reasons to use Pryme Space. Here are a few:</p>
+          <h6>Make money</h6>
+          <ul>
+            <li>We turn your space into cash. Having more streams of revenue is always a good thing.</li>
+          </ul>
 
-          <p>Host payouts will be deposited into the bank account they have on file with us via ACH transfer (direct deposit) and are processed within 7 days of the start of the reservation.</p>
+          <h6>All-in-one Platform</h6>
+          <ul>
+            <li>Pryme Space is an easy to use, all-in-one platform, for hosts to list and earn extra income for their space and for users to find spaces that saves them valuable time and effort.</li>
+          </ul>
 
+          <h6>Largest database of spaces</h6>
+          <ul>
+            <li>We provide free or low-cost visibility of spaces to users worldwide, and quick and seamless cash flow to hosts by integrating IoT into our platform to create and connect smart cities and regions</li>
+          </ul>
+
+          <h6>AI for best matches</h6>
+          <ul>
+            <li>Our unique set of intelligent AI tools enable users to save time by instantly finding the perfect property. Review systems and other protections on our platform enable both hosts and users the peace of mind to share or use their space.</li>
+          </ul>
+
+          <h6>A competitive marketplace</h6>
+          <ul>
+            <li>Our wide range of spaces ensure the best price and market recommendations for every budget. Our hosts can use our smart pricing features or set their own pricing. Our spaces range so users find exactly what suits their needs and budget.</li>
+          </ul>
       </div>
-
       </div>
-
       </div>
-
-
-
-      <div class="card">
-
-      <div class="card-header" role="tab" id="questionFive">
-
-      <h5 class="card-title">
-
-      <a class="collapsed" data-toggle="collapse" data-parent="#faq" href="#answerFive" aria-expanded="true" aria-controls="answerFive">
-
-          Does Pryme Space provide any tax information like a Form 1099-K?
-
-      </a>
-
-      </h5>
-
       </div>
+  </div><!-- #general-faq -->
+  <div id="host-faq" class="tab-pane fade">
+    <h3>Menu 1</h3>
+    <p>Some content in menu 1.</p>
+  </div><!-- #host-faq -->
+  <div id="renter-faq" class="tab-pane fade">
+    <h3>Menu 2</h3>
+    <p>Some content in menu 2.</p>
+  </div><!-- #renter-faq -->
+  <div id="pricing-faq" class="tab-pane fade">
+    <h3>Menu 2</h3>
+    <p>Some content in menu 2.</p>
+  </div><!-- #pricing-faq -->
+  <div id="payments-faq" class="tab-pane fade">
+    <h3>Menu 2</h3>
+    <p>Some content in menu 2.</p>
+  </div><!-- #payments-faq -->
+  <div id="other-faq" class="tab-pane fade">
+    <h3>Menu 2</h3>
+    <p>Some content in menu 2.</p>
+  </div><!-- #other-faq -->
+</div>
 
-      <div id="answerFive" class="collapse in" role="tabcard" aria-labelledby="questionFive">
-
-      <div class="card-body">
-
-          <p>The US Internal Revenue Service (IRS) requires US companies that process payments, including Pryme Space, to report gross earnings for all US users who earn over $20,000 and have 200+ transactions in the calendar year. If you exceed both IRS thresholds in a calendar year, Pryme Space will issue you a Form 1099-K. If Pryme Space withholds income tax from your earnings, they will also send a form indicating how much was withheld.</p>
-
-      </div>
-
-      </div>
-
-      </div>
-
-
-
-      </div>
+      
 
   </div>
-
  </section>
 
 
