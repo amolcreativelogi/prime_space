c9aa5b45192028de6cdfc59259fd35edaadee183
unknown <amol.creativelogi@gmail.com>
2019-05-07 15:56:34 -0500

    faq updated code added for sequence issue

--- resources/views/front/cms_pages/view.blade.php
+++ resources/views/front/cms_pages/view.blade.php
@@ -1,15 +1,23 @@
 @extends('front/layouts.default')
 @section('content')
 <div class="site-content">
-@if(!empty($getCMSPageData))
+<?php 
+    $description="This page is not available";
+	if(!empty($getCMSPageData) && (($getCMSPageData->status != 0) && ($getCMSPageData->is_deleted != 1))){
+
+		$description=(isset($getCMSPageData->description) && !empty($getCMSPageData->description))?$getCMSPageData->description:"";
+    }
+?>
 <section class="dl-content aboutpage">
+@if(!empty($getCMSPageData))
   <div class="container">
     <h2 class="dash-title">  <?php echo empty($getCMSPageData->title)?"":$getCMSPageData->title; ?></h2>
     <div class="static-content">
-      	<?php echo empty($getCMSPageData->description)?"":$getCMSPageData->description; ?>
+      	<?php echo $description; ?>
     </div>
   </div>
+ @endif
 </section>
-@endif
+
 </div>
 @stop
